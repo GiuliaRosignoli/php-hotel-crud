a1afaa14f05ed6409eb72e53b822c52bf9725a4f
GiuliaRosignoli <giuliarosignoli@gmail.com>
2021-05-28 16:53:46 +0200

    .

--- index.php
+++ index.php
@@ -1,38 +1,12 @@
-<?php
-/* Database connection */
+<!DOCTYPE html>
+<html lang="en">
+<?php 
+require_once __DIR__ . '/partials/head.php'; ?>
 
-// Connection info
+<body>
+    <main>
+        <h1>Customer archive</h1>
+    </main>
 
-$host = 'localhost';
-$username = 'root';
-$password = '';
-$db_name = 'hotel';
-
-// Connection
-
-$conn = new mysqli($host, $username, $password, $db_name);
-var_dump($conn);
-
-// Check connection
-if($conn && $conn->connect_error){
-    die("Connection failed: $conn->connect_error ");
-}
-
-// echo '<h2>DB Connected</h2>';
-
-// Get DB data
-
-$sql = "SELECT `room_number`, `floor` FROM `stanze`";
-$result = $conn->query($sql);
-
-if($result && $result->num_rows > 0){
-    while($row = $result->fetch_assoc()) {
-        var_dump($row);
-    }
-}
-
-
-
-
-
-?>
+</body>
+</html>
---
+++ partials/database.php
@@ -0,0 +1,43 @@
+<?php
+/* Database connection */
+
+// Connection info
+
+$host = 'localhost';
+$username = 'root';
+$password = '';
+$db_name = 'hotel';
+
+// Connection
+
+$conn = new mysqli($host, $username, $password, $db_name);
+var_dump($conn);
+
+// Check connection
+if($conn && $conn->connect_error){
+    die("Connection failed: $conn->connect_error ");
+}
+
+// echo '<h2>DB Connected</h2>';
+
+// Get DB data
+
+$sql = "SELECT `room_number`, `floor` FROM `stanze` WHERE `id = 7`";
+$result = $conn->query($sql);
+
+if($result && $result->num_rows > 0){
+    while($row = $result->fetch_assoc()) {
+       // var_dump($row);
+       echo '<h2>Stanza n: ' . $row['room_number'] . 'piano: ' . $row['floor'] . '<h2>';
+    }
+} elseif($result){
+    echo '<h2>No results found </h2>';
+} else {
+    echo 'query error';
+}
+
+
+
+
+
+?>
