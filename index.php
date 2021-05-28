84d48b3175d8c503586176b1eb9cbcba749c9e45
GiuliaRosignoli <giuliarosignoli@gmail.com>
2021-05-28 15:57:20 +0200

    db connected

--- index.php
+++ index.php
@@ -1,9 +1,24 @@
 <?php
 /* Database connection */
 
+// Connection info
 
+$host = 'localhost';
+$username = 'root';
+$password = '';
+$db_name = 'hotel';
 
+// Connection
 
+$conn = new mysqli($host, $username, $password, $db_name);
+var_dump($conn);
+
+// Check connection
+if($conn && $conn->connect_error){
+    die("Connection failed:$conn->connect_error ");
+}
+
+echo '<h2>DB Connected</h2>';
 
 
 
