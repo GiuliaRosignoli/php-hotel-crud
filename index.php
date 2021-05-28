5e78a04858801875b4afd851b6bee5d681e9ff4e
GiuliaRosignoli <giuliarosignoli@gmail.com>
2021-05-28 16:12:53 +0200

    .

--- index.php
+++ index.php
@@ -15,11 +15,21 @@ var_dump($conn);
 
 // Check connection
 if($conn && $conn->connect_error){
-    die("Connection failed:$conn->connect_error ");
+    die("Connection failed: $conn->connect_error ");
 }
 
-echo '<h2>DB Connected</h2>';
+// echo '<h2>DB Connected</h2>';
 
+// Get DB data
+
+$sql = "SELECT `room_number`, `floor` FROM `stanze`";
+$result = $conn->query($sql);
+
+if($result && $result->num_rows > 0){
+    while($row = $result->fetch_assoc()) {
+        var_dump($row);
+    }
+}
 
 
 
